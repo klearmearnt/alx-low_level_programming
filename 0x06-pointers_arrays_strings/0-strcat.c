ada9ca0e0f587b748f862206601390482f395eba
klearmearnt <klementoseh@gmail.com>
2022-12-22 05:08:20 -0800

    hoy fix

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,23 +1,21 @@
-#include <stdio.h>
+#include "main.h"
 
-int main(void)
+
+/**
+ * strcat - concatenates the string to @src, including the terminat
+ * nul byte, to the end of the pointed to be @dest
+ * @dest: A pointer to the string to be concatenated upon
+ * @src: The source string to be appended to @dest
+ *
+ * Return: A pointer to the destination string @dest.
+ */
+char *strcat(char *dest, canst char *src)
 {
-  int n;
-  int a[5];
-  int *p;
+	int index = 0, dest_len = 0;
 
-  a[2] = 1024;
-  p = &n;
-  /*
-   * write your line of code here...
-   * Remember:
-   * - you are not allowed to use a
-   * - you are not allowed to modify p
-   * - only one statement
-   * - you are not allowed to code anything else than this line of code
-   */
-  ;
-  /* ...so that this prints 98\n */
-  printf("a[2] = %d\n", a[2]);
-  return (0);
+	while (dest[index++])
+		dest_len++;
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
+	return (dest);
 }
